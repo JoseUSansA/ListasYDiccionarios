8db934c9b97423e73eea80827400719651cb1f6a
JoseUSansA <jo.aranedaf@duocuc.cl>
2026-06-03 12:41:56 -0400

    Ejercicio funcional sin validaciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,13 @@
-#No estuve en las listas así que jaja XD
+contador_comandantes = 0
+contador_cadete = 0
+cantidad_pilotos = int(input("Ingrese cantidad de pilotos: "))
 
+for i in range(cantidad_pilotos):
+    nombre = input("Ingrese nombre del piloto: ")
+    nivel = int(input("Ingrese nivel del piloto: "))
+
+    if nivel > 40:
+        contador_comandantes += 1
+    else:
+        contador_cadete += 1
+print(f"¡La flota estelar cuenta con {contador_comandantes} Comandantes Galácticos y {contador_cadete} Cadetes Estelares!")
